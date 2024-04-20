5730a9bcc255f0ecda395409eb768136a9fcbda4
Baylee Kimmel <bkimmel1226@gmail.com>
2024-04-19 22:46:43 -0400

    CRUD fully functional, some styling

--- controllers/patients.js
+++ controllers/patients.js
@@ -36,7 +36,12 @@ router.get('/:id', async (req, res) => {
 })
 
 // EDIT //
-
+router.get('/:id/edit', async (req, res) => {
+    const foundPatient = await Patient.findById(req.params.id)
+    res.render('edit.ejs', {
+        patient: foundPatient
+    })
+})
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -50,6 +55,26 @@ router.post('/', async (req, res) => {
         console.log(err)
         res.status(500).send(err.message)
     }
+})
+
+// UPDATE (PUT) //
+router.put('/:id', async (req, res) => {
+    try {
+        req.body.insurance = req.body.insurance ? true : false
+        const updatedPatient = await Patient.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        console.log(updatedPatient)
+        res.redirect(`/patients/${req.params.id}`)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err.message)
+    }
+})
+
+// DELETE //
+router.delete('/:id', async (req, res) => {
+    const patient = await Patient.findByIdAndDelete(req.params.id)
+    console.log(`Patient ${patient.name} successfully deleted!`)
+    res.redirect('/patients')
 })
 
 module.exports = router
